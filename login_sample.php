be6fe7c6816227fc51b6dd7792613281642d691a
Arkadi Listov <arkalis@greeny.cs.tlu.ee>
2015-09-23 11:54:56 +0300

    login create

--- login_sample.php
+++ login_sample.php
@@ -1,5 +1,12 @@
 <?php
 
+	//loo ab-i ühenduse
+	require_once("../config.php");
+	$database = "if15_arkadi_3";
+	$mysqli = new mysqli ($servername, $username, $password, $database);
+	
+	
+	
   // muuutujad errorite jaoks
 	$email_error = "";
 	$password_error = "";
@@ -36,8 +43,26 @@
       // Kui oleme siia jõudnud, võime kasutaja sisse logida
 			if($password_error == "" && $email_error == ""){
 				echo "Võib sisse logida! Kasutajanimi on ".$email." ja parool on ".$password;
+				
+				$hash = hash("sha512", $password);
+				
+				$stmt = $mysqli->prepare("SELECT id, email FROM user_sample WHERE email=? AND password=?");
+				$stmt->bind_param("ss", $email, $hash);
+				
+				//muutujad tulemustele
+				$stmt->bind_result($id_from_db, $email_from_db);
+				$stmt->execute();
+				
+				//kontrolli kas tulemusi leiti
+				if($stmt->fetch()){	
+					echo "Email ja parool on õiged, kasutaja id=".$id_from_db;
+				}else{
+					echo "Wrong credentials!";
+				}
 			}
-
+			
+			$stmt->close();
+			
 		} // login if end
 
     // *********************
@@ -62,8 +87,17 @@
 			}
 
 			if(	$create_email_error == "" && $create_password_error == ""){
-				echo "Võib kasutajat luua! Kasutajanimi on ".$create_email." ja parool on ".$create_password;
-      }
+				
+				$hash = hash("sha512", $create_password);
+				
+				echo "Võib kasutajat luua! Kasutajanimi on ".$create_email." ja parool on ".$create_password." ja räsi on ".$hash;
+				
+				$stmt = $mysqli->prepare("INSERT INTO user_sample (email, password) VALUES (?,?)");
+				$stmt->bind_param("ss", $create_email, $hash);
+				$stmt->execute();
+				$stmt->close();
+      }			
+				
 
     } // create if end
 
@@ -77,6 +111,8 @@
   	return $data;
   }
 
+	//paneme ühenduse kinni
+	$mysqli->close();
 ?>
 <!DOCTYPE html>
 <html>
